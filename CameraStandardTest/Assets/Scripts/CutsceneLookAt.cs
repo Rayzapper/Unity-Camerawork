5e4cd9c7e19225f1b85bb4c7dba223477435d94c
Rasmus <rasmus.lindberg.93@gmail.com>
2018-04-22 23:01:39 +0200

    Basic

---
+++ CameraStandardTest/Assets/Scripts/CutsceneLookAt.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneLookAt : MonoBehaviour
+{
+    public GameObject cameraTarget;
+    public Camera cam;
+    public bool smoothingActive = false;
+    public float smoothSpeed = 0.01f, deadZoneX = 0.3f, deadZoneY = 0.3f;
+
+    private float fov, hFov;
+    private bool camLock = false;
+    private Plane leftPlane, rightPlane, topPlane, bottomPlane;
+    //private Vector3 offset;
+
+    private void Start()
+    {
+        
+    }
+
+    public void OnEnable()
+    {
+        camLock = true;
+    }
+
+    public void OnDisable()
+    {
+        camLock = false;
+    }
+
+    public void LateUpdate()
+    {
+        if (camLock)
+        {
+            fov = cam.fieldOfView;
+            hFov = fov * cam.aspect;
+
+            Vector3 deltaVector = cameraTarget.transform.position - cam.transform.position;
+            Vector3 targetScreenPosition = cam.WorldToScreenPoint(cameraTarget.transform.position);
+            if (targetScreenPosition.x < Screen.width * deadZoneX || targetScreenPosition.x > Screen.width * (1 - deadZoneX) ||
+                targetScreenPosition.y < Screen.height * deadZoneY || targetScreenPosition.y > Screen.height * (1 - deadZoneY))
+            {
+                cam.transform.forward = Vector3.Slerp(cam.transform.forward, deltaVector, 0.05f);
+            }
+
+            //leftPlane = new Plane(cam.transform.position, Vector3.Slerp(cam.transform.right, cam.transform.forward, (hFov / 2) / 90));
+            //rightPlane = new Plane(cam.transform.position, Vector3.Slerp(-cam.transform.right, cam.transform.forward, (hFov / 2) / 90));
+            //topPlane = new Plane(cam.transform.position, Vector3.Slerp(-cam.transform.up, cam.transform.forward, (fov / 2) / 90));
+            //bottomPlane = new Plane(cam.transform.position, Vector3.Slerp(cam.transform.up, cam.transform.forward, (fov / 2) / 90));
+
+            //Plane hPlane = new Plane(cam.transform.position, cam.transform.up);
+            //Plane vPlane = new Plane(cam.transform.position, cam.transform.right);
+
+            //Vector3 deltaVector = cameraTarget.transform.position - cam.transform.position;
+
+            //if (!leftPlane.GetSide(cameraTarget.transform.position))
+            //{
+            //    if (!rightPlane.GetSide(cameraTarget.transform.position))
+            //    {
+            //        cam.transform.forward = Vector3.Slerp(cam.transform.forward, deltaVector, smoothSpeed);
+            //    }
+            //}
+            //if (!topPlane.GetSide(cameraTarget.transform.position))
+            //{
+            //    if (!bottomPlane.GetSide(cameraTarget.transform.position))
+            //    {
+            //        cam.transform.forward = Vector3.Slerp(cam.transform.forward, deltaVector, smoothSpeed);
+            //    }
+            //}
+        }
+    }
+}
---
+++ CameraStandardTest/Assets/Scripts/TestingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    public float angle = 0;
+
+    public void OnEnable()
+    {
+        Debug.Log("Object enabled");
+    }
+
+    public void OnDisable()
+    {
+        Debug.Log("Object disabled");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.Slerp(transform.forward, transform.up, angle / 90) * 10);
+    }
+}
