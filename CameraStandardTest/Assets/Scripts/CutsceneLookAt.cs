18b42a899efe167919a48ad9ef6a6ed675fdcacc
Rasmus <rasmus.lindberg.93@gmail.com>
2018-04-23 14:39:28 +0200

    ghghk

--- CameraStandardTest/Assets/Scripts/CutsceneLookAt.cs
+++ CameraStandardTest/Assets/Scripts/CutsceneLookAt.cs
@@ -6,73 +6,51 @@ public class CutsceneLookAt : MonoBehaviour
 {
     public GameObject cameraTarget;
     public Camera cam;
-    public bool smoothingActive = false;
-    public float smoothSpeed = 0.01f, deadZoneX = 0.3f, deadZoneY = 0.3f;
+    public bool resetMode = false;
+    public float smoothSpeed = 2.0f, deadZoneX = 0.3f, deadZoneY = 0.3f;
 
     //private float fov, hFov;
     private bool camLock = false;
-    private Plane leftPlane, rightPlane, topPlane, bottomPlane;
-    //private Vector3 offset;
-
-    private void Start()
-    {
-        
-    }
+    private Vector3 startPos;
+    private Quaternion startRot;
 
     public void OnEnable()
     {
         camLock = true;
+        if (cam != null)
+        {
+            startPos = cam.transform.position;
+            startRot = cam.transform.rotation;
+        }
     }
 
     public void OnDisable()
     {
         camLock = false;
+        if (resetMode)
+            if (cam != null)
+            {
+                cam.transform.position = startPos;
+                cam.transform.rotation = startRot;
+            }
     }
 
     public void LateUpdate()
     {
         if (camLock)
         {
-            //fov = cam.fieldOfView;
-            //hFov = fov * cam.aspect;
-
             Vector3 deltaVector = cameraTarget.transform.position - cam.transform.position;
             Vector3 targetScreenPosition = cam.WorldToScreenPoint(cameraTarget.transform.position);
             if (targetScreenPosition.x < Screen.width * deadZoneX || targetScreenPosition.x > Screen.width * (1 - deadZoneX) ||
                 targetScreenPosition.y < Screen.height * deadZoneY || targetScreenPosition.y > Screen.height * (1 - deadZoneY))
             {
                 float angleDelta, angleFactor, distanceDelta, distanceFactor = 1.0f;
-                angleDelta = Vector3.Angle(cam.transform.forward, targetScreenPosition);
+                angleDelta = Vector3.Angle(cam.transform.forward, deltaVector);
                 distanceDelta = Vector3.Distance(cam.transform.position, cameraTarget.transform.position);
                 angleFactor = Mathf.Abs(angleDelta) / 90;
                 distanceFactor = 40 / distanceDelta;
-                cam.transform.forward = Vector3.Slerp(cam.transform.forward, deltaVector, Time.deltaTime * 1.5f * angleFactor * distanceFactor);
+                cam.transform.forward = Vector3.Slerp(cam.transform.forward, deltaVector, Time.deltaTime * smoothSpeed * angleFactor * distanceFactor);
             }
-
-            //leftPlane = new Plane(cam.transform.position, Vector3.Slerp(cam.transform.right, cam.transform.forward, (hFov / 2) / 90));
-            //rightPlane = new Plane(cam.transform.position, Vector3.Slerp(-cam.transform.right, cam.transform.forward, (hFov / 2) / 90));
-            //topPlane = new Plane(cam.transform.position, Vector3.Slerp(-cam.transform.up, cam.transform.forward, (fov / 2) / 90));
-            //bottomPlane = new Plane(cam.transform.position, Vector3.Slerp(cam.transform.up, cam.transform.forward, (fov / 2) / 90));
-
-            //Plane hPlane = new Plane(cam.transform.position, cam.transform.up);
-            //Plane vPlane = new Plane(cam.transform.position, cam.transform.right);
-
-            //Vector3 deltaVector = cameraTarget.transform.position - cam.transform.position;
-
-            //if (!leftPlane.GetSide(cameraTarget.transform.position))
-            //{
-            //    if (!rightPlane.GetSide(cameraTarget.transform.position))
-            //    {
-            //        cam.transform.forward = Vector3.Slerp(cam.transform.forward, deltaVector, smoothSpeed);
-            //    }
-            //}
-            //if (!topPlane.GetSide(cameraTarget.transform.position))
-            //{
-            //    if (!bottomPlane.GetSide(cameraTarget.transform.position))
-            //    {
-            //        cam.transform.forward = Vector3.Slerp(cam.transform.forward, deltaVector, smoothSpeed);
-            //    }
-            //}
         }
     }
 }
